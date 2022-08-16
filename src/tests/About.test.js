4a71f612920e1d445b325358aa15fc0d265eb525
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-08-16 17:30:31 -0300

    Requisito 2 concluído

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router';
+import { About } from '../pages';
+
+describe('Testando o componente <About.js />', () => {
+  test('É exibido na tela um h2 com texto About Pokédex', () => {
+    // acessar
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <About />
+      </Router>,
+    );
+    const text = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+
+    // aferir
+    expect(text).toBeInTheDocument();
+  });
+  test('O atributo src da imagem é o esperado', () => {
+    // acessar
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <About />
+      </Router>,
+    );
+    const image = screen.getByRole('img');
+
+    // aferir
+    expect(image.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
