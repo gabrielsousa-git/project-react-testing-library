3e8bd82a43d3aa216e6dfbc2dc3f9c0738bbac8f
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-08-16 17:05:32 -0300

    Requisito 1 concluído

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router';
+import App from '../App';
+
+describe('Testando o componente <App.js />', () => {
+  test('É exibido na tela um link com o texto Home', () => {
+    // acessar
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const linkHome = screen.getByRole('link', { name: /Home/i });
+
+    // aferir
+    expect(linkHome).toBeInTheDocument();
+  });
+  test('É exibido na tela um link com o texto About', () => {
+    // acessar
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+
+    // aferir
+    expect(linkAbout).toBeInTheDocument();
+  });
+  test('É exibido na tela um link com o texto Favorite Pokémons', () => {
+    // acessar
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+
+    // aferir
+    expect(linkFavorite).toBeInTheDocument();
+  });
+});
