0fdd82a2f5917be705cd895bb2b253250d318d32
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-08-16 22:53:41 -0300

    Requisito 3 concluído

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router';
+import { FavoritePokemons } from '../pages';
+
+describe('Testando o componente <FavoritePokemons.js />', () => {
+  test('É exibido na tela a mensagem No favorite pokemon found', () => {
+    // acessar
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <FavoritePokemons />
+      </Router>,
+    );
+    const text = screen.getByText('No favorite pokemon found');
+
+    // aferir
+    expect(text).toBeInTheDocument();
+  });
+});
